32253ff2e706d439118eb1cb6c120a45687f758c
Ari Efendi <ariefendi992@gmail.com>
2023-05-18 12:30:53 +0800

    add guru home

--- lib/views/guru/main_guru_page.dart
+++ lib/views/guru/main_guru_page.dart
@@ -1,6 +1,9 @@
 import 'package:absen_siswa_qr_code/cubit/auth/auth_cubit.dart';
 import 'package:absen_siswa_qr_code/cubit/page/page_cubit.dart';
 import 'package:absen_siswa_qr_code/utils/theme.dart';
+import 'package:absen_siswa_qr_code/views/guru/menu/guru_home_page.dart';
+import 'package:absen_siswa_qr_code/views/guru/menu/profil_guru_page.dart';
+import 'package:absen_siswa_qr_code/views/widgets/button_bottom_app_bar.dart';
 import 'package:absen_siswa_qr_code/views/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,39 +13,65 @@ class MainGuruPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocConsumer<AuthCubit, AuthState>(
-        listener: (context, state) {
-          if (state is AuthFailed) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                backgroundColor: kErrorColor,
-                content: Text(
-                  state.error,
-                  style: TextStyle(
-                    fontWeight: medium,
-                    fontSize: 14,
-                  ),
-                ),
+    Widget content(currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return GuruHomePage();
+        case 1:
+          return ProfilGuruPage();
+
+        default:
+          return GuruHomePage();
+      }
+    }
+
+    Widget bottomNavigation() {
+      return Container(
+        height: 58,
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ButtonAppBarWidget(
+                title: 'Beranda',
+                index: 0,
+                iconPrimary: Icons.home,
+                iconSecondary: Icons.home_outlined,
               ),
-            );
-          } else if (state is AuthInitial) {
-            context.read<PageCubit>().setPage(0);
-            Navigator.pushNamedAndRemoveUntil(
-                context, '/auth', (route) => false);
-          }
-        },
-        builder: (context, state) {
-          return Center(
-            child: ButtonWidget(
-              title: 'Keluar',
-              onTap: () {
-                context.read<AuthCubit>().logOut();
-              },
+              ButtonAppBarWidget(
+                title: 'Akun',
+                index: 1,
+                iconPrimary: Icons.account_circle,
+                iconSecondary: Icons.account_circle_outlined,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            elevation: 6,
+            clipBehavior: Clip.hardEdge,
+            color: kWhiteColor,
+            child: IconTheme(
+              data: IconThemeData(
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              child: bottomNavigation(),
             ),
-          );
-        },
-      ),
+          ),
+          body: Stack(
+            children: [
+              content(currentIndex),
+            ],
+          ),
+        );
+      },
     );
   }
 }
