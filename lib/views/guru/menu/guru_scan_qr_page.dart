9736f5eda9bb4bd0a3625d22eddf2dbe6e138979
Ari Efendi <ariefendi992@gmail.com>
2023-05-21 11:28:17 +0800

    update ui scan qr page

--- lib/views/guru/menu/guru_scan_qr_page.dart
+++ lib/views/guru/menu/guru_scan_qr_page.dart
@@ -1,6 +1,6 @@
 import 'package:absen_siswa_qr_code/cubit/page/page_cubit.dart';
 import 'package:absen_siswa_qr_code/utils/theme.dart';
-import 'package:flutter/foundation.dart';
+import 'package:absen_siswa_qr_code/views/widgets/screen_cam_overlay.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -13,32 +13,46 @@ class GuruScanPage extends StatefulWidget {
 }
 
 class _GuruScanPageState extends State<GuruScanPage> {
-  bool isScanCompleted = false;
-  MobileScannerController cameraController = MobileScannerController();
-  bool screenOpened = false;
-  String nisn = '';
+  MobileScannerController cameraController = MobileScannerController(
+      detectionSpeed: DetectionSpeed.noDuplicates, detectionTimeoutMs: 5000);
+  bool torchOn = false;
+  bool frontCamera = false;
+  bool cameraOpened = false;
 
-  void closeScreen() {
-    isScanCompleted = false;
-  }
+  void onCamera(capture) {
+    List<Barcode> barcodes = capture.barcodes;
 
-  void screenWasClosed() {
-    screenOpened = false;
+    if (!cameraOpened)
+      for (final barcode in barcodes) {
+        print('Barcode ===> [$barcode]');
+        print(barcode.rawValue);
+        // cameraOpened = true;
+        final String code = barcode.rawValue ?? '___';
+        debugPrint(code);
+
+        // setState(() {});
+      }
   }
 
   @override
   void initState() {
     super.initState();
-    this.screenWasClosed();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cameraController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(60),
         child: AppBar(
-          backgroundColor: primary,
+          backgroundColor: Colors.transparent,
           foregroundColor: kWhiteColor,
           elevation: 0,
           title: Text(
@@ -56,6 +70,11 @@ class _GuruScanPageState extends State<GuruScanPage> {
                   context.read<PageCubit>().setPage(0);
                   Navigator.pushNamedAndRemoveUntil(
                       context, '/mainGuru', (route) => false);
+                  setState(() {
+                    cameraOpened = false;
+                    print(cameraOpened);
+                  });
+                  cameraController.stop();
                 },
                 icon: Icon(
                   Icons.arrow_back_ios_new,
@@ -65,76 +84,115 @@ class _GuruScanPageState extends State<GuruScanPage> {
           ),
         ),
       ),
-      body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
+      body: Stack(
+        children: [
+          MobileScanner(
+            controller: cameraController,
+            onDetect: onCamera,
+            fit: BoxFit.cover,
+            startDelay: true,
+          ),
+          QRScannerOverlay(
+            overlayColour: Colors.transparent.withOpacity(0.6),
+          ),
+          Column(
+            children: [
+              Expanded(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(
-                      'Place the QR Code in the area',
-                      style: TextStyle(fontSize: 18, fontWeight: bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Scanning wil be started automatically',
-                      style: TextStyle(fontSize: 16),
+                    Container(
+                      margin: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).size.height / 6.0),
+                      child: Text(
+                        'Scan QR Code di sini',
+                        style: TextStyle(
+                          color: kWhiteColor,
+                          fontSize: 16,
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Stack(
-                children: [
-                  MobileScanner(
-                    controller: MobileScannerController(
-                      detectionSpeed: DetectionSpeed.normal,
-                      // facing: CameraFacing.front,
-                      torchEnabled: false,
-                      autoStart: true,
-                    ),
-                    onDetect: foundBarcode,
-                  )
-                ],
+              Expanded(
+                child: Container(),
               ),
-            ),
-            Expanded(
-              child: Container(
-                alignment: Alignment.center,
-                child: Text(
-                  nisn,
-                  style: TextStyle(
-                    color: kBlackColor,
+              Expanded(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.only(top: 10),
+
+                  // alignment: Alignment.center,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: !frontCamera
+                                ? () {
+                                    setState(() {
+                                      torchOn = !torchOn;
+                                    });
+
+                                    cameraController.toggleTorch();
+                                  }
+                                : () {},
+                            icon: Icon(
+                              torchOn == false
+                                  ? Icons.flash_on_rounded
+                                  : Icons.flash_off_rounded,
+                              color: !frontCamera
+                                  ? kWhiteColor
+                                  : kWhiteColor.withOpacity(0.5),
+                              size: 36,
+                            ),
+                          ),
+                          Text(
+                            torchOn ? 'Flash OFF' : 'Flash ON',
+                            style: TextStyle(
+                              color: kWhiteColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 32),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                frontCamera = !frontCamera;
+                              });
+
+                              cameraController.switchCamera();
+                            },
+                            icon: Icon(
+                              frontCamera == false
+                                  ? Icons.camera_front_rounded
+                                  : Icons.camera_rear_rounded,
+                              color: kWhiteColor,
+                              size: 36,
+                            ),
+                          ),
+                          Text(
+                            torchOn ? 'Front Camera' : 'Rear Camera',
+                            style: TextStyle(
+                              color: kWhiteColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
-  }
-
-  void foundBarcode(capture) {
-    print('CAPTURE ==>> [$capture]');
-
-    final List<Barcode> barcodes = capture.barcodes;
-
-    if (!screenOpened) {
-      for (final barcode in barcodes) {
-        print('NISN FOUND ${barcode.rawValue}');
-        print('TYPE ===> [${barcode.displayValue}]');
-        final String code = barcode.rawValue ?? '___';
-        setState(() {
-          nisn = '${barcode.rawValue}';
-        });
-      }
-    }
   }
 }
